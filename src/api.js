fbdf167f93f72e39887de11f3abcfd3b36e4f4ee
James Geneser <jamesgeneser@gmail.com>
2023-08-31 10:58:05 -0600

    search bar and geocities api framework

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5aab3c488emshdbff1078958e942p1f24e0jsn4c2c0c70c8f5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
