4843c028996286e1e56a1aaf763282f30d646297
James Geneser <jamesgeneser@gmail.com>
2023-08-31 13:44:41 -0600

    fetch from open weather successful, forecast commented out

--- src/App.js
+++ src/App.js
@@ -7,29 +7,34 @@ import "./App.css";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
+  // const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     console.log(searchData);
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeahterFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid=${WEATHER_API_KEY}`
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     );
 
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}$appid=${WEATHER_API_KEY}`
-    );
+    // const forecastFetch = fetch(
+    //   `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}$appid=${WEATHER_API_KEY}`
+    // );
 
-    Promise.all([currentWeahterFetch, forecastFetch]).then(async (response) => {
-      const weatherResponse = await response[0].json();
-      const forecastResponse = await response[1].json();
+    Promise.all([currentWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        // const forecastResponse = await response[1].json();
 
-      setCurrentWeather(weatherResponse);
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
 
-      setForecast(forecastResponse);
-    });
+        // setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
+
+  console.log(currentWeather);
+  // console.log(forecast);
 
   return (
     <div className="container">
