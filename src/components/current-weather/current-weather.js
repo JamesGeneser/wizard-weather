7e89d8ae091f661afc2995984e0aefa1054b9f7d
James Geneser <jamesgeneser@gmail.com>
2023-09-07 12:52:07 -0600

    switch component deleted, template literal used to dynamically call assets

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,10 +1,9 @@
 import "./current-weather.css";
 import { Container, Row, Col } from "react-bootstrap";
-import Switch from "../switch";
 
 const CurrentWeather = ({ data }) => {
-  const conditions = JSON.stringify(data.weather[0].description);
-  console.log(conditions);
+  //   const conditions = JSON.stringify(data.weather[0].description);
+  //   console.log(conditions);
   return (
     <Container fluid>
       <img
@@ -12,10 +11,11 @@ const CurrentWeather = ({ data }) => {
         className="crystal-ball"
       />
 
-      {conditions && <Switch data={conditions} />}
-      {/* <img src={require("../../media/Sunny.png")} className="sunny"></img> */}
-      {/* <img src={require("../../media/Cloudy.png")} className="cloudy" />
-      <img src={require("../../media/Cloudy.png")} className="cloudy" /> */}
+      <img
+        src={require(`../../media/${data.weather[0].icon}.png`)}
+        className="icon"
+      />
+
       <Row className="current-weather">
         <Col className="city-name">{data.city}</Col>
         <Col className="weather-conditions">{data.weather[0].description}</Col>
